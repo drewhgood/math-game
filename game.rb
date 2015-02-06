d01686ba2f099c04d00ded3869d92f463aca9bfb
drewhgood <drewhgood@gmail.com>
2015-02-06 07:43:47 +0000

    Added userto be prompted with random addition question. Properly evaluates if response is correct or not.

--- game.rb
+++ game.rb
@@ -1,5 +1,29 @@
-#this is game
+@num = rand(10)
+@num2 = rand(10)
+@whos_turn = 1
 
-def test
+
+def ask_question
+
+  puts "what is #{@num} + #{@num2} ?"
+
+  puts "Player #{@whos_turn}, please give your answer"
+  response = gets.chomp.to_i 
+
+  compare(response)
 
 end
+
+
+
+def compare(response) 
+
+  if response == @num+@num2
+    p 'right'
+  else
+    p 'wrong'
+  end
+
+end
+
+ask_question

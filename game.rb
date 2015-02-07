420ea21cc50f6d769ec817c4e9031438ccfbb943
drewhgood <drewhgood@gmail.com>
2015-02-07 23:29:22 +0000

    Challend completed. Cleaned up formatting and updated player variable names.

--- game.rb
+++ game.rb
@@ -26,52 +26,38 @@ class Player
     @name = name 
   end
 
-
 end
-
-
-
-
 
 
 def initialize_game(need_new_names)
 
- 
-
   if need_new_names
 
-    @p1 = Player.new
-    @p2 = Player.new
+    @player_1 = Player.new
+    @player_2 = Player.new
 
     p'Before we get started, I need your names.'
 
     puts 'What is your name, player 1?'
     
     n1 = gets.chomp
-    @p1.set_name(n1) 
+    @player_1.set_name(n1) 
     puts 'What is your name, player 2?'
     n2 = gets.chomp
-    @p2.set_name(n2) 
-
-
+    @player_2.set_name(n2) 
 
   end
 
 
-
-   @p1.reset_lives_remaining
-   @p2.reset_lives_remaining
-   @whos_turn = 1
-
-
+  @player_1.reset_lives_remaining
+  @player_2.reset_lives_remaining
+  @whos_turn = 1
   generate_question
   
 end
 
 
-
  @whos_turn = 1 #1 represents player 1
-
 
 
 def generate_question
@@ -81,7 +67,7 @@ def generate_question
   @answer = n1 + n2
 
   
-  question= "#{@whos_turn == 1 ? @p1.name : @p2.name}, what is #{n1} + #{n2} ?"
+  question= "#{@whos_turn == 1 ? @player_1.name : @player_2.name}, what is #{n1} + #{n2} ?"
   
   ask_question(question)
 end
@@ -104,19 +90,18 @@ def evaluate_response(response)
  update_state(correct)
 end
 
+
 def update_state(correct)
   
   if correct == false
-    
-    @whos_turn == 1 ?  @p1.lives_remaining -= 1 : @p2.lives_remaining -= 1
-
+    @whos_turn == 1 ?  @player_1.lives_remaining -= 1 : @player_2.lives_remaining -= 1
   end
 
   #update turn
   @whos_turn == 1 ? @whos_turn = 2 : @whos_turn = 1
 
   #check if game over
-  if @p1.lives_remaining == 0 || @p2.lives_remaining == 0
+  if @player_1.lives_remaining == 0 || @player_2.lives_remaining == 0
     end_game
   else
     scoreboard
@@ -127,16 +112,16 @@ end
 
 def scoreboard
 
-  p "#{@p1.name}: (#{@p1.lives_remaining})pts | Player #{@p2.name}: (#{@p2.lives_remaining})pts"
+  p "#{@player_1.name}: (#{@player_1.lives_remaining})pts | Player #{@player_2.name}: (#{@player_2.lives_remaining})pts"
   generate_question
 
 end
 
 
 def end_game
-   p @p1.lives_remaining == 0 ? "#{@p2.name} wins!"  : "#{@p1.name} wins!"
-   @p1.lives_remaining == 0 ? @p2.add_win  : @p1.add_win
 
+   p @player_1.lives_remaining == 0 ? "#{@player_2.name} wins!"  : "#{@player_1.name} wins!"
+     @player_1.lives_remaining == 0 ? @player_2.add_win  : @player_1.add_win
 
    cumulative_scoreboard
 
@@ -155,12 +140,8 @@ end
 
 def cumulative_scoreboard
   # add point when player wins a game
-  p "This sessions score: #{@p1.name} - #{@p1.cumulative_score}(wins)  |  #{@p2.name} - #{@p2.cumulative_score}(wins)"
-
+  p "This sessions score: #{@player_1.name} - #{@player_1.cumulative_score}(wins)  |  #{@player_2.name} - #{@player_2.cumulative_score}(wins)"
 end
-
-
-
 
 
  initialize_game(true)

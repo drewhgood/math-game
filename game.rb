12425dda8cb6a3602514dd0dd0974ea1a4797b1f
Andrew Good <drewhgood@gmail.com>
2015-03-05 13:25:59 -0800

    Ask question, gather response, and evaluate response methods added.

--- game.rb
+++ game.rb
@@ -1,7 +1,37 @@
-def generate_question
-  number_1 = rand(20)
-  number_2 = rand(20)
-  @answer = number_1 + number_2
+x = 1
+@p1_lives = 3
+@p2_lives = 3
 
-  print "What is  #{number_1} + #{number_2}?"  
+
+def generate_question
+  @number_1 = rand(20)
+  @number_2 = rand(20)
+  @answer = @number_1 + @number_2  
+end
+
+
+def ask_question
+  print "What is  #{@number_1} + #{@number_2}?"
+end
+
+
+def gather_response
+  resonse = gets.chomp.to_i
+end
+
+
+def evalutate_response(response)
+  p response == @answer
+end
+
+
+
+
+while x > 0
+  generate_question
+  ask_question 
+  evalutate_response(gather_response) 
+  
+
+  x = 0
 end
